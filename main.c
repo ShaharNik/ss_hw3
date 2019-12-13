e2f4f1672d0cab63c67f4da3f1ca781dcfd8a82e
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2019-12-13 19:50:00 +0200

    Part 1 upload - merge sort

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    //insertion_sort(arr, n);
+   // printArray(arr, n);
+    //printf("%d", n);
+    int arr2[] = { 12, 11, 13, 5, 6, 0 };
+    printArray(arr2, 6);
+    shift_element(arr2, 3);
+    printArray(arr2, 6);
+    return 0;
+}

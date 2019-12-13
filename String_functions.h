0ddbda46e6abc37ea412fb6fb40842071778bb1a
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2019-12-13 19:48:17 +0200

    Added First String Functions

---
+++ String_functions.h
@@ -0,0 +1,14 @@
+//
+// Created by Niknazar on 12/13/2019.
+//
+
+#ifndef EX3_NEW_STRING_FUNCTIONS_H
+#define EX3_NEW_STRING_FUNCTIONS_H
+int getline(char s[]);
+int getword(char w[]);
+int substring(char *str1, char *str2);
+int similar(char *s, char *t, int n);
+
+void print_lines(char *str);
+void print_similar_words(char *str);
+#endif //EX3_NEW_STRING_FUNCTIONS_H

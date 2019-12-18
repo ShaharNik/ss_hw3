26206b2d5cb7cc7122ff8a9a85569bfe7b3e169a
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2019-12-18 17:57:48 +0200

    Update and rename merge_sort.h to insertion_sort.h

--- Ex3/insertion_sort.h
+++ Ex3/insertion_sort.h
@@ -2,10 +2,10 @@
 // Created by shaharnik on 17/12/2019.
 //
 
-#ifndef SS_HW3_MERGE_SORT_H
-#define SS_HW3_MERGE_SORT_H
+#ifndef SS_HW3_INSERTION_SORT_H
+#define SS_HW3_INSERTION_SORT_H
 
 void shift_element (int* arr , int i);
 void insertion_sort(int* arr , int len);
 void printArray(int arr[], int n);
-#endif //SS_HW3_MERGE_SORT_H
+#endif //SS_HW3_INSERTION_SORT_H
